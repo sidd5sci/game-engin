c9490911c55e90476b2cd0166c5cd86b76ad8d74
sidd5sci <sidd5sci@gmail.com>
2017-12-24 23:01:53 +0530

    data

--- GA.py
+++ GA.py
@@ -23,46 +23,46 @@ class population:
 class GA:
     def __init__(self):
         #======================================
-        Population = []
-        initialPopulation = 10
-        genration = 0
-        mutationRate = 10 # in percentage
-        crossOverRate = 2 # in percentage
+        self.Population = []
+        self.initialPopulation = 10
+        self.genration = 0
+        self.mutationRate = 10 # in percentage
+        self.crossOverRate = 2 # in percentage
         #======================================
       
     def createRandomPopulation(self,n):
         for i in range(0,n):
                 p = population()
                 p.randomize()
-                Population.append(p)
+                self.Population.append(p)
         
     def Genration(self):
-        genration += 1
+        self.genration += 1
             
     def naturalSelection(self,task,**args):
         # run the GA simulation in physical world
-        for i in range(0,len(Population)):
-            fit(Population[i],task,**args)# pass the fintness function into this
+        for i in range(0,len(self.Population)):
+            fit(self.Population[i],task,**args)# pass the fintness function into this
             # by default fitness is depends on the time taken by the creature to perform the task
-        for i in range(0,len(Population)):
-            if Population[i].health < 20:
-                Population[i].state = 'dead'
+        for i in range(0,len(self.Population)):
+            if self.Population[i].health < 20:
+                self.Population[i].state = 'dead'
 
                 
     def ShowFitness(self):
                 
-        for i in range(0,initialPopulation):
-            Population[i].CalFitness()
+        for i in range(0,self.initialPopulation):
+            self.Population[i].CalFitness()
             
     def findBestFitesest(self):
-        parent1 = parent2 = Population[0]
-        for i in range(1,initialPopulation):
+        parent1 = parent2 = self.Population[0]
+        for i in range(1,self.initialPopulation):
             #print(parent1.fitness," | ",parent2.fitness," || ",Population[i].fitness)
-            if parent1.fitness < Population[i].fitness:
+            if parent1.fitness < self.Population[i].fitness:
                 parent2 = parent1
-                parent1 = Population[i]
-            elif parent2.fitness < Population[i].fitness:
-                parent2 = Population[i]
+                parent1 = self.Population[i]
+            elif parent2.fitness < self.Population[i].fitness:
+                parent2 = self.Population[i]
     def crossOverEngine(self):
         pass
     def mutation(self):
